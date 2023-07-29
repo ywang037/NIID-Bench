0080d96c4a4b8b7420490847d7a441c0f20afed2
Frank Wang <32762885+ywang037@users.noreply.github.com>
2023-07-29 23:11:44 +0800

    Update run_moon.sh

--- run_moon.sh
+++ run_moon.sh
@@ -2,22 +2,26 @@ for dataset in cifar10 cifar100 fmnist
 do
 	for beta in 0.02 0.05 0.1 0.2
 	do
-		CUDA_VISIBLE_DEVICES=2 python experiments_moon.py \
-			--model=convnet \
-			--dataset=$dataset \
-			--alg=moon \
-			--lr=0.01 \
-			--batch-size=64 \
-			--epochs=10 \
-			--n_parties=10 \
-			--rho=0.9 \
-			--comm_round=2 \
-			--partition=noniid-labeldir \
-			--beta=$beta \
-			--device='cuda' \
-			--datadir='./data/' \
-			--logdir='./logs/' \
-			--noise=0 \
-			--init_seed=42
+		for seed in 42 43 45
+		do
+			CUDA_VISIBLE_DEVICES=2 python experiments_moon.py \
+				--model=convnet \
+				--dataset=$dataset \
+				--alg=moon \
+				--lr=0.01 \
+				--batch-size=64 \
+				--epochs=10 \
+				--n_parties=10 \
+				--rho=0.9 \
+				--comm_round=2 \
+				--partition=noniid-labeldir \
+				--beta=$beta \
+				--device='cuda' \
+				--datadir='./data/' \
+				--logdir='./logs/' \
+				--noise=0 \
+				--init_seed=$seed
+			sleep 5m
+		done 
 	done
 done
